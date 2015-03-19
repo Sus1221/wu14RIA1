9c13acaac5eb34b228db5d49914639a14d76b7eb
BengtAndersson <abengt@live.com>
2015-03-19 15:56:26 +0100

    Dagens verk!

--- wp-content/themes/ngTheme-master/js/services/propfactory.js
+++ wp-content/themes/ngTheme-master/js/services/propfactory.js
@@ -1,4 +1,4 @@
-app.factory ('PropFactory', ["WPRest", function(WPRest){
+app.factory ('PropFactory', ["WPRest", "$sce", function(WPRest, $sce){
 
 	//WPRest.restCall("/", "GET", {}, "WPRestWorks");
 	 //factory code will go here...
@@ -18,7 +18,49 @@ app.factory ('PropFactory', ["WPRest", function(WPRest){
       }
       */
 
-      WPRest.restCall(callUrl, "GET", {}, "Banan");
+      WPRest.restCall(callUrl, "GET", {}, {
+      	broadcastName: "notImportant",
+      	callback: function(postData){
+
+      		console.log("Vi har hittat poster: ", postData);
+
+      		var searchResult = [];
+
+      		var i = 0;
+      		postData.forEach(function(post) {
+      			//last visar om vi är på den sista i loopen
+      			var last = i ===postData.length-1;
+
+      			if (!post.terms.proptaxonomy) { return;}
+
+      			post.excerpt = $sce.trustAsHtml(post.excerpt);
+            	post.content = $sce.trustAsHtml(post.content);
+
+            	
+            	var propertyTag = post.terms.proptaxonomy[0].slug;
+            	var mediaCallUrl = "/media?filter[proptaxonomy]="+propertyTag;
+
+            	WPRest.restCall(mediaCallUrl, "GET", {}, {
+            		//this broadcast is VERY important
+	              broadcastName: last ? "foundProperty" : "notDone", 
+	              callback: function(mediaData) {
+	                //callback is triggered when we get data but 
+	                //BEFORE we broadcast data throughout the app	 
+	                //just log data
+	                console.log("Property found property media: ", mediaData); 
+	              }
+	            });
+
+            	i++;
+
+
+
+      		});
+
+      	}
+
+
+      });
 
       console.log("Property find method will now call REST url: ", callUrl);
 
