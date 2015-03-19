273aa688ccb41a755cc6b198a3fb6fc06d2e2afe
Electromotif <pjhk@yahoo.com>
2015-03-19 15:03:39 +0100

    solved conflicts

--- wp-content/themes/ngTheme-master/js/controllers/homeController.js
+++ wp-content/themes/ngTheme-master/js/controllers/homeController.js
@@ -1,14 +1,8 @@
 //"ngTheme" home controller.
 //dependent on $scope && WPService being injected to run
 
-app.controller("homeController", ["$scope", "Pages", "$sce", "Property", function($scope, Pages, $sce, Property) {
+app.controller("homeController", ["$scope", "Pages", "$sce", "PropFactory", function($scope, Pages, $sce, PropFactory) {
   console.log("homeController alive!");
-<<<<<<< HEAD
-  
-
-  
-=======
->>>>>>> 49616ea37fee2e7927e1f4b981a24fa5de685882
 
   //get all pages
   Pages.get();
