d478a934b5ecbb4ca924a0c99743739bb37106a8
Sus1221 <susanne881221@hotmail.com>
2015-03-24 14:12:33 +0100

    managed to print propertyinfo to html-partial

--- wp-content/themes/ngTheme-master/js/controllers/propertyController.js
+++ wp-content/themes/ngTheme-master/js/controllers/propertyController.js
@@ -6,9 +6,9 @@ app.controller("propertyController", ["$scope", "PropFactory" ,"$routeParams", f
   //broadcast is coming from Property factory 
   $scope.$on("foundProperty", function(event, data) {
     console.log("propertyController on foundProperty: ", data);
-    if (data.length === 0) { 
-      console.log("No data "); 
-      return; 
+    if (data.length === 0) {
+      console.log("No data ");
+      return;
     }
     $scope.property = data;
   });
