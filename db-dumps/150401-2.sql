04f149512302d7324f9252565d4368144412478d
Electromotif <pjhk@yahoo.com>
2015-04-01 14:24:53 +0200

    databasdum med meny

---
+++ db-dumps/150401-2.sql
@@ -0,0 +1,67 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Värd: 127.0.0.1 (MySQL 5.6.20)
+# Databas: wu14ria1
+# Genereringstid: 2015-04-01 12:24:22 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Tabelldump wp_terms
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `wp_terms`;
+
+CREATE TABLE `wp_terms` (
+  `term_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL DEFAULT '',
+  `slug` varchar(200) NOT NULL DEFAULT '',
+  `term_group` bigint(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`term_id`),
+  KEY `slug` (`slug`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `wp_terms` WRITE;
+/*!40000 ALTER TABLE `wp_terms` DISABLE KEYS */;
+
+INSERT INTO `wp_terms` (`term_id`, `name`, `slug`, `term_group`)
+VALUES
+	(1,'Uncategorized','uncategorized',0),
+	(2,'fastigheter','fastigheter',0),
+	(3,'1','1',0),
+	(4,'2','2',0),
+	(5,'3','3',0),
+	(6,'45','45',0),
+	(7,'ob1','ob1',0),
+	(8,'ob2','ob2',0),
+	(9,'ob3','ob3',0),
+	(10,'ob4','ob4',0),
+	(11,'ob5','ob5',0),
+	(12,'Meny 1','meny-1',0),
+	(13,'About','about',0);
+
+/*!40000 ALTER TABLE `wp_terms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
