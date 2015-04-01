30724e21526860ebcd5ce10bbc2eb05a9e589fb8
Sus1221 <susanne881221@hotmail.com>
2015-04-01 13:33:20 +0200

    minor change to filter

--- wp-content/themes/ngTheme-master/js/filters/price.js
+++ wp-content/themes/ngTheme-master/js/filters/price.js
@@ -13,7 +13,7 @@ app.filter('price', [function(){
 
 		for(var i = 0; i < properties.length; i++) {
 			var p = properties[i];
-			propPriceAsNo = p.facts.asking_price / 1;
+			var propPriceAsNo = p.facts.asking_price / 1;
 		
 			if(range[0] && range[1] && propPriceAsNo >= range[0] && propPriceAsNo <= range[1]) {
 				propsThatGotThrough.push(p);
